5f257cb4b1f7a61ca4ff5f524c8d95a80224f0f7
AMIT <rs.mauryarocks@gmail.com>
2025-05-26 21:06:42 +0530

    working on startDialKnob

--- Assets/Scripts/ForkliftController.cs
+++ Assets/Scripts/ForkliftController.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VRTemplate;
 using UnityEngine;
 
 public class ForkliftController : MonoBehaviour
 {
-    [SerializeField] private 
+    [SerializeField] private XRKnob engineStartKnob;
+
     void Start()
     {
-        
+        engineStartKnob.onValueChange.AddListener(EngineStart);
     }
 
+    private void EngineStart(float val)
+    {
+        Debug.Log("Start the engine : " + val);
+    }
    
 }
