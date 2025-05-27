f81aadd678d96db05baa56b545d8527d8bcf3640
AMIT <rs.mauryarocks@gmail.com>
2025-05-27 17:45:19 +0530

    cam lift carton with lift

--- Assets/Scripts/ForkliftController.cs
+++ Assets/Scripts/ForkliftController.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 
 public class ForkliftController : MonoBehaviour
 {
-  
+    [SerializeField] private Transform target;
 
     void Start()
     {
-       
+
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.TryGetComponent<Carton>(out Carton component))
+        {
+            Transform carton = collision.transform;  
+            carton.SetParent(target);
+            carton.GetComponent<Rigidbody>().isKinematic = true;
+            carton.localPosition = Vector3.zero;
 
-   
+            Debug.Log("collided elemnet name : " + collision.transform.name);
+        }
+        else
+        {
+            Debug.Log("collided elemnet name : other carton type");
+        }
+    }
+    
+
+
+
+
 }
