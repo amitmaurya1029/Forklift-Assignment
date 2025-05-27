2cf14bd38482f7cfb3e0b91958cf0f2a7d01e3ab
AMIT <rs.mauryarocks@gmail.com>
2025-05-27 13:11:20 +0530

    added lift up and down sound

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    [SerializeField] private AudioSource forkliftSound;
+    [SerializeField] private AudioSource forkliftEngine;
+    [SerializeField] private AudioSource forkliftLift;
+    [SerializeField] private AudioClip liftUp;
+    [SerializeField] private AudioClip liftDown;
+    
+
     void Start()
     {
         StartEngineDial.OnForkliftStart += PlayEngineStartSound;
@@ -15,15 +20,28 @@ public class SoundController : MonoBehaviour
     {
         if (e.HasEngineStart())
         {
-            forkliftSound.Play();
+            forkliftEngine.Play();
             Debug.Log("Engine Sound start : active");
         }
         else
         {
-            forkliftSound.Stop();
+            forkliftEngine.Stop();
             Debug.Log("Engine Sound start : disable");
         }
     }
 
+
+    public void PlayLiftUpSound()
+    {
+        forkliftLift.clip = liftUp;
+        forkliftLift.Play();
+    }
+
+    
+    public void PlayLiftDownSound()
+    {
+        forkliftLift.clip = liftDown;
+        forkliftLift.Play();
+    }
  
 }
